e16e369b8a705e2a054b327659487af878a16319
Christian Eduardo Mendoza López <chritianmern@gmail.com>
2025-08-04 21:28:35 -0600

    Parte 3 de la historia

--- historia.py
+++ historia.py
@@ -2,3 +2,5 @@ print("Parte 1: Juan salió de su casa muy temprano.")
 
 print("Parte 2: Se encontró con un dragón en la entrada del bosque.")  # Axel Andres Beltran Silva
 
+print("Parte 3: Al final, el dragón resultó ser su mascota perdida.")
+
